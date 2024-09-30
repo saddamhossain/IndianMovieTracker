b47e3b1edf8c9a9c96ed48c67cbf0e1756e7eb1a
saddamhossain <saddamhossain953@gmail.com>
2024-09-30 16:04:04 +0600

    cleanup

--- IndianMovieTracker/Startup.cs
+++ IndianMovieTracker/Startup.cs
@@ -2,16 +2,10 @@ using IndianMovieTracker.Data;
 using IndianMovieTracker.Interface;
 using IndianMovieTracker.Service;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IndianMovieTracker
 {
